8f3108bfbf1ddba285245342e363e8ed205c7490
Grant Chang <70448709+granthchang@users.noreply.github.com>
2022-10-10 22:52:54 -0700

    Continued refactor

--- Constants/Settings.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace MahjongConstants {
-  [CreateAssetMenu(menuName = "Settings")]
-  public abstract class Settings : ScriptableObject {
-    public readonly int StartingScore;
-    public readonly int MinimumFans;
-  }
-}
